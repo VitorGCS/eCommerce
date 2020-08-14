47cb53744f2c6f92381b922df4ac1a47d5b56663
Vitor Silva <vitorgilcs@gmail.com>
2020-08-14 20:05:35 +0100

    eCommerce Project - Angular Front End - Product List

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+   
+  products:Product[];
+  //inject our ProductService
+  constructor(private productService: ProductService) { }
 
-  constructor() { }
-
+  //simillar to @PostConstruct in Spring
   ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts(){
+    this.productService.getProductList().subscribe(
+      data => {this.products = data;}
+    )
   }
 
 }
