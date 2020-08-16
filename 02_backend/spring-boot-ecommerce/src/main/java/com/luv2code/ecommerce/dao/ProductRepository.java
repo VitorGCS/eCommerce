22976b162a0a78b7e7af54d18f0f3732a3030a42
Vitor Silva <vitorgilcs@gmail.com>
2020-08-16 18:52:01 +0100

    Search for Product by category - update Spring Boot

--- 02_backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02_backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,19 @@
 package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /*
+     * Behind the scenes, Spring will execute a query similar to this: select * from product where category_id=?
+     * Spring Data REST automatically expose endpoint: http://localhost:8080/api/products/search/findByCategory?id=2
+     * */
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
 }
