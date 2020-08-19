9cc56bf6d43709d57bf1b91c3d2533a7361fee5c
Vitor Silva <vitorgilcs@gmail.com>
2020-08-19 14:02:01 +0100

    Enhance menu component to read data from product service - update product service to call URL on Spring Boot app

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,13 +3,15 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators'
+import { ProductCategory } from '../common/product-category';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
-  private baseUrl = 'http://localhost:8080/api/products';
   
+  private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]>{
@@ -17,14 +19,29 @@ export class ProductService {
     //need to build URL based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products )
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    //Call REST API
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory )
+    );
+    //Returns an observable, maps the JSON data from Spring Data REST to ProductCategory array
+  }
 }
 
-interface GetResponse{
+interface GetResponseProducts{
     _embedded: {
       products: Product[];
     }
+}
+
+//Unwraps the JSON from Spring Data REST _embedded entry
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategory: ProductCategory[];
+  }
 }
