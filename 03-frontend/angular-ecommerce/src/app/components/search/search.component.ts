bdb723adc0bc951eeb791f039635a6cb9301edef
Vitor Silva <vitorgilcs@gmail.com>
2020-08-20 18:02:32 +0100

    Update Search Component to send data to search route

--- 03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  
+  //first: Inject the router
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  doSearch(value: string){
+    console.log(`value=${value}`);
+    this.router.navigateByUrl(`/search/${value}`);
+    //Route the data to our "search" route it will be handled by the ProductListComponent 0> to reuse the logic and view for listing products
   }
 
 }
