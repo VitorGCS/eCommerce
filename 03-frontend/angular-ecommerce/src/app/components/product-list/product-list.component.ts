1516633031058fdf9f32cb145cab8919ddb7f88d
Vitor Silva <vitorgilcs@gmail.com>
2020-08-20 18:11:40 +0100

    Enhance ProductListComponent to search for products with ProductService

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -6,39 +6,48 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  //templateUrl: './product-list-table.component.html',
-  //templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-   
-  products:Product[];
+
+  products: Product[];
   currentCategoryId: number;
   currentCategoryName: string;
+  searchMode: boolean;
 
   //inject our ProductService
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+    private route: ActivatedRoute) { }
 
   //simillar to @PostConstruct in Spring
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
-    
+
   }
 
-  listProducts(){
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if(this.searchMode){
+      this.handleSearchProducts();
+    }else{
+      this.handleListProducts();
+    }
+    
 
+  }
+
+  handleListProducts() {
     //Check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
-    
-    if(hasCategoryId){
+
+    if (hasCategoryId) {
       //get the "id" param string. convert to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
       //get the "name" param string
       this.currentCategoryName = this.route.snapshot.paramMap.get('name');
-    }else{
+    } else {
       //not category id available.... default to category id 1
       this.currentCategoryId = 1;
       this.currentCategoryName = 'Books';
@@ -46,7 +55,18 @@ export class ProductListComponent implements OnInit {
 
     //now get the products for the given category id
     this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {this.products = data;}
+      data => { this.products = data; }
+    )
+  }
+
+  handleSearchProducts(){
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    //now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
     )
   }
 
