38e1ba727f1c3d4d37700b616007a8ec15e42d0e
Vitor Silva <vitorgilcs@gmail.com>
2020-08-23 19:19:29 +0100

    corrections nedded about working pagination

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+								  
 import { Product } from '../common/product';
-import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ProductCategory } from '../common/product-category';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,42 +13,49 @@ export class ProductService {
 
 
   private baseUrl = 'http://localhost:8080/api/products';
+
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
   getProduct(theProductId: number): Observable<Product> {
-    //need to build URL based on product id
+
+    // need to build URL based on product id
     const productUrl = `${this.baseUrl}/${theProductId}`;
 
     return this.httpClient.get<Product>(productUrl);
   }
 
-  getProductCategories(): Observable<ProductCategory[]> {
-    //Call REST API
-    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
-      map(response => response._embedded.productCategory)
-    );
-    //Returns an observable, maps the JSON data from Spring Data REST to ProductCategory array
+  getProductListPaginate(thePage: number, 
+                         thePageSize: number, 
+                         theCategoryId: number): Observable<GetResponseProducts> {
+
+    // need to build URL based on category id, page and size 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-    //need to build URL based on category id 
+
+    // need to build URL based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.getProducts(searchUrl);
   }
 
-  getProductListPaginate(thePage:number, thePageSize: number, theCategoryId: number): Observable<GetResponseProducts> {
-    //need to build URL based on category id, page and size - Spring Data REST supports pagination out of the box (just send the parameter for page and size)
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}&page=${thePage}$size=${thePageSize}`;
+																													   
+																																							 
+																																								
 
-    return this.httpClient.get<GetResponseProducts>(searchUrl);
-  }
+															   
+   
 
   searchProducts(theKeyword: string): Observable<Product[]> {
-    //need to build URL based on the keyword
+
+    // need to build URL based on the keyword 
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
     return this.getProducts(searchUrl);
@@ -54,11 +63,17 @@ export class ProductService {
 
 
   private getProducts(searchUrl: string): Observable<Product[]> {
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(map(response => response._embedded.products));
+												  
+	  
   }
 
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 
 }
 
@@ -67,14 +82,14 @@ interface GetResponseProducts {
     products: Product[];
   },
   page: {
-    size: number, //size of this page
-    totalElements: number, //grand total of ALL elements in the database
-    totalPages: number, //total pages available
-    number: number //current page number
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
-//Unwraps the JSON from Spring Data REST _embedded entry
+														
 interface GetResponseProductCategory {
   _embedded: {
     productCategory: ProductCategory[];
