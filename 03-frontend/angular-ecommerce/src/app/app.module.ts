5f8fca24cff42651455ef7336e54227b0b54b759
Vitor Silva <vitorgilcs@gmail.com>
2020-08-25 19:40:38 +0100

    Create new component: CartStatusComponent and add HTML template for CartStatusComponent

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductCategoryMenuComponent } from './components/product-category-menu
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
 
 const routes: Routes = [
   {path: 'products/:id', component: ProductDetailsComponent},
@@ -27,7 +28,8 @@ const routes: Routes = [
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartStatusComponent
   ],
   imports: [ //Exposes the exported declarations (classes, interfaces, constants etc) and makes them avaliable in the current module
     RouterModule.forRoot(routes),
