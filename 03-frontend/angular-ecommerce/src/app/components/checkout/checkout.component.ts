7d0341b6011a31e221e2cb2b0c0aa6f3076ae331
Vitor Silva <vitorgilcs@gmail.com>
2020-08-30 13:34:31 +0100

    eCommerce Project - Checkout Form - Drop-down Lists

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 
 @Component({
   selector: 'app-checkout',
@@ -21,6 +22,9 @@ export class CheckoutComponent implements OnInit {
 
   countries: Country[] = [];
 
+  shippingAddresStates: State[] = [];
+  billingAddresStates: State[] = [];
+  
 
   //inject the form builder
   constructor(private formBuilder: FormBuilder,
@@ -90,8 +94,15 @@ export class CheckoutComponent implements OnInit {
   copyShippingAddressToBillingAddress(event){
     if(event.target.checked){
       this.checkoutFormGroup.controls.billingAddress.setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+
+      //bug fix for states
+      this.billingAddresStates = this.shippingAddresStates;
+
     }else{
       this.checkoutFormGroup.controls.billingAddress.reset();
+
+      //bug fix for states
+      this.billingAddresStates = [];
     }
   }
 
@@ -99,6 +110,8 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("The email address is "+this.checkoutFormGroup.get('customer').value.email);
+    console.log("The shipping address country is "+this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address state is "+this.checkoutFormGroup.get('shippingAddress').value.state.name);
   }
 
   handleMonthsAndYears(){
@@ -122,6 +135,28 @@ export class CheckoutComponent implements OnInit {
         this.creditCardMonths = data;
       }
 
+    );
+  }
+
+  getStates(formGroupName: string){
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code ${countryCode}`);
+    console.log(`${formGroupName} country name ${countryName}`);
+
+    this.luv2ShopFormServices.getStates(countryCode).subscribe(
+      data => {
+        if(formGroupName === 'shippingAddress'){
+          this.shippingAddresStates = data;
+        }else{
+          this.billingAddresStates = data;
+        }
+
+        //select first item by default
+        formGroup.get('state').setValue(data[0])
+      }
     );
   }
 
