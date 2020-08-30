e0018a10931f7624f47049addbdd3b60078074c5
Vitor Silva <vitorgilcs@gmail.com>
2020-08-30 12:33:43 +0100

    Create TypeScript classes ofr Contry and State and add methods to the form service for countries and states

--- 03-frontend/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Country } from '../common/country';
+import { map } from 'rxjs/operators';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Luv2ShopFormService {
 
-  constructor() { }
+  private countriesUrl = "http://localhost:8080/api/countries";
+  private statesUrl = "http://localhost:8080/api/states";
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  getCountries(): Observable<Country[]>{
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries )
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]>{
+
+    //search url
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
+
+
 
   getCreditCardMonths(startMonth: number): Observable<number[]>{
     let data: number[] = [];
@@ -33,5 +59,19 @@ export class Luv2ShopFormService {
     }
     // The "of" operator from rxjs will wrap an object as an Observable
     return of(data);
+  }
+}
+
+//unwraps the JSON form Spring Data REST _embedded entry
+interface GetResponseCountries{
+  _embedded:{
+    countries: Country[];
+  }
+}
+
+
+interface GetResponseStates{
+  _embedded:{
+    states: State[];
   }
 }
